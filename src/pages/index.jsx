16bcaa4540c4f914da0e1218d401c426c8d9c445
RS0627 <neko0627@gmail.com>
2023-04-30 12:39:43 +0900

    ⚡️useCallbackの引数動作を確認

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,25 @@
 import Head from "next/head";
 import { Main } from "@/src/components/Main";
 import { Header } from "@/src/components/Header";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [cnt, setCnt] = useState(1);
 
-  function handleClick(e) {
-    e.preventDefault();
-    setCnt(function (inputCnt) {
-      return inputCnt + 1;
-    });
-  }
+  const handleClick = useCallback(
+    (e) => {
+      console.log(cnt);
+      if (cnt < 10) {
+        setCnt(function (inputCnt) {
+          return inputCnt + 1;
+        });
+      }
+    },
+    [cnt]
+  );
 
   useEffect(() => {
+    console.log("effect");
     document.body.style.backgroundColor = "lightGreen";
 
     return () => {
