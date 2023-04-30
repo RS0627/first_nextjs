39b5c5c11c6e92f50e226d416e02c509e5998af9
RS0627 <neko0627@gmail.com>
2023-04-30 11:40:21 +0900

    ✨ useStateを使った状態管理をボタンカウンターで確認。

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,18 @@
 import Head from "next/head";
 import { Main } from "@/src/components/Main";
 import { Header } from "@/src/components/Header";
-import { useEffect } from "react";
-
-function handleClick(e) {
-  console.log(e.target.href);
-  e.preventDefault();
-}
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [cnt, setCnt] = useState(1);
+
+  function handleClick(e) {
+    e.preventDefault();
+    setCnt(function (inputCnt) {
+      return inputCnt + 1;
+    });
+  }
+
   useEffect(() => {
     document.body.style.backgroundColor = "lightGreen";
 
@@ -20,11 +24,10 @@ export default function Home() {
   return (
     <>
       <Header />
-
-      <a href="./about" onClick={handleClick}>
+      <h1>{cnt}</h1>
+      <button href="./about" onClick={handleClick}>
         ボタン
-      </a>
-
+      </button>
       <Main page="index" />
     </>
   );
