82dd904c28e8ffd38936ad63fc5cbb1ae8505e90
RS0627 <neko0627@gmail.com>
2023-04-30 11:03:21 +0900

    ✨ useEffectの機能追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,29 @@
 import Head from "next/head";
 import { Main } from "@/src/components/Main";
 import { Header } from "@/src/components/Header";
+import { useEffect } from "react";
+
+function handleClick(e) {
+  console.log(e.target.href);
+  e.preventDefault();
+}
 
 export default function Home() {
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightGreen";
+
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
+
   return (
     <>
       <Header />
+
+      <a href="./about" onClick={handleClick}>
+        ボタン
+      </a>
 
       <Main page="index" />
     </>
