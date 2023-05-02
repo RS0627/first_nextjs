05a0e102b8b4b3cbd9eb86b6260ea58b30d2e84d
RS0627 <neko0627@gmail.com>
2023-05-02 10:05:46 +0900

    ♻️ 引数をpropsに変更

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,29 +2,22 @@ import styles from "@/src/styles/Home.module.css";
 import { Main } from "@/src/components/Main";
 import { Header } from "@/src/components/Header";
 
-export default function About({
-  doubleCnt,
-  isShow,
-  handleClick,
-  handleDisplay,
-  text,
-  array,
-  handleTextChange,
-  handleAdd,
-}) {
+export default function About(props) {
   return (
     <div className={styles.container}>
       <Header />
-      {isShow ? <h1>{doubleCnt}</h1> : null}
-      <button href="./about" onClick={handleClick}>
+      {props.isShow ? <h1>{props.doubleCnt}</h1> : null}
+      <button href="./about" onClick={props.handleClick}>
         ボタン
       </button>
-      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
-      <input type="text" value={text} onChange={handleTextChange} />
-      <button onClick={handleAdd}>追加</button>
+      <button onClick={props.handleDisplay}>
+        {props.isShow ? "非表示" : "表示"}
+      </button>
+      <input type="text" value={props.text} onChange={props.handleTextChange} />
+      <button onClick={props.handleAdd}>追加</button>
 
       <ul>
-        {array.map((item) => {
+        {props.array.map((item) => {
           return <li kye={item}>{item}</li>;
         })}
       </ul>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,30 +3,21 @@ import { Main } from "@/src/components/Main";
 import { Header } from "@/src/components/Header";
 
 export default function Home(props) {
-  const {
-    cnt,
-    isShow,
-    handleClick,
-    handleDisplay,
-    text,
-    array,
-    handleTextChange,
-    handleAdd,
-  } = props;
-
   return (
     <div className={styles.container}>
       <Header />
-      {isShow ? <h1>{cnt}</h1> : null}
-      <button href="./about" onClick={handleClick}>
+      {props.isShow ? <h1>{props.cnt}</h1> : null}
+      <button href="./about" onClick={props.handleClick}>
         ボタン
       </button>
-      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
-      <input type="text" value={text} onChange={handleTextChange} />
-      <button onClick={handleAdd}>追加</button>
+      <button onClick={props.handleDisplay}>
+        {props.isShow ? "非表示" : "表示"}
+      </button>
+      <input type="text" value={props.text} onChange={props.handleTextChange} />
+      <button onClick={props.handleAdd}>追加</button>
 
       <ul>
-        {array.map((item) => {
+        {props.array.map((item) => {
           return <li kye={item}>{item}</li>;
         })}
       </ul>
